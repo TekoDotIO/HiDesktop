5e2e45cf11bae575da1c54e0380463bc8ee34b6e
1003 fengye <mcfengye1003@outlook.com>
2022-05-02 23:06:58 +0800

    backup

---
+++ HiDesktop/TextBar.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace HiDesktop
+{
+    partial class TextBar
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.LabelNo1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // LabelNo1
+            // 
+            this.LabelNo1.AutoSize = true;
+            this.LabelNo1.Font = new System.Drawing.Font("Microsoft YaHei UI", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.LabelNo1.Location = new System.Drawing.Point(0, 0);
+            this.LabelNo1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.LabelNo1.Name = "LabelNo1";
+            this.LabelNo1.Size = new System.Drawing.Size(69, 35);
+            this.LabelNo1.TabIndex = 0;
+            this.LabelNo1.Text = "Text";
+            this.LabelNo1.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.OnMouseUp);
+            this.LabelNo1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.OnMouseDown);
+            this.LabelNo1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.OnMouseUp);
+            // 
+            // TextBar
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(100, 48);
+            this.ControlBox = false;
+            this.Controls.Add(this.LabelNo1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.ImeMode = System.Windows.Forms.ImeMode.Disable;
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "TextBar";
+            this.ShowInTaskbar = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.Manual;
+            this.Text = "TextBar";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label LabelNo1;
+    }
+}
---
+++ HiDesktop/TextBar.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Text;
+using System.IO;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Windows.Forms;
+
+namespace HiDesktop
+{
+    public partial class TextBar : Form
+    {
+        readonly string Path;
+        readonly Hashtable AppConfig;
+        public TextBar(string Path)
+        {
+            InitializeComponent();
+            this.Path = Path;
+            Hashtable htStandard = new Hashtable()
+            {
+                { "type", "TextBar" },
+                { "font", "auto" },
+                { "fontSize", "30" },
+                { "opacity", "1" },
+                { "topMost", "true" },
+                { "text", "Configue your textBar in properties file.." },
+                { "location", "auto" },
+                { "enabled","true" },
+                { "color","#FFFFFF" },
+                { "allowMove","true" }
+            };
+            if (!File.Exists(Path))
+            {
+                Hashtable Config = htStandard;
+                PropertiesHelper.Save(Path, Config);
+            }
+            if (File.ReadAllText(Path) == "")
+            {
+                Hashtable Config = htStandard;
+                PropertiesHelper.Save(Path, Config);
+            }
+            PropertiesHelper.FixProperties(htStandard, Path);
+            AppConfig = PropertiesHelper.Load(Path);
+
+            if ((string)AppConfig["enabled"] != "true")
+            {
+                Log.SaveLog($"{Path}已被禁用");
+                this.Close();
+                return;
+
+            }
+
+            if ((string)AppConfig["type"] != "CounterBar")
+            {
+                Log.SaveLog($"{Path}不是一个文本窗口的配置文件,已跳过加载.");
+                this.Close();
+                return;
+            }
+
+            float fontSize = Convert.ToInt32(AppConfig["fontSize"]);
+            Opacity = Convert.ToDouble(AppConfig["opacity"]);
+            if ((string)AppConfig["topMost"] == "true")
+            {
+                TopMost = true;
+            }
+            else
+            {
+                TopMost = false;
+            }
+            if ((string)AppConfig["font"] != "auto")
+            {
+                Setfont((string)AppConfig["font"]);
+            }
+            LabelNo1.ForeColor = ColorTranslator.FromHtml((string)AppConfig["color"]);
+            LabelNo1.Font = new Font(LabelNo1.Font.Name, fontSize);
+
+        }
+
+        public void Setfont(string path)
+        {
+            try
+            {
+                //从外部文件加载字体文件
+                PrivateFontCollection font = new PrivateFontCollection();
+                font.AddFontFile(path);
+
+                //定义成新的字体对象
+                FontFamily myFontFamily = new FontFamily(font.Families[0].Name, font);
+                Font myFont = new Font(myFontFamily, 56F, FontStyle.Regular);
+
+                //将字体显示到控件
+                LabelNo1.Font = myFont;
+
+            }
+            catch (InvalidCastException e)
+            {
+                MessageBox.Show(e.Message.ToString(), "异常：", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+            }
+        }
+
+        [DllImport("user32.dll")]
+        public static extern bool ReleaseCapture();
+
+        [DllImport("user32.dll")]
+        public static extern bool SendMessage(IntPtr hwnd, int wMsg, int wParam, int lParam);
+
+        private void FrmMain_MouseDown(object sender, MouseEventArgs e)
+        {
+            if ((string)AppConfig["allowMove"] == "true")
+            {
+                ReleaseCapture();
+                SendMessage(this.Handle, 0x0112, 0xF012, 0);
+            }
+
+        }
+        protected override void OnMouseDown(MouseEventArgs e)
+        {
+            base.OnMouseDown(e);
+            FrmMain_MouseDown(this, e);
+        }
+        protected override void OnMouseUp(MouseEventArgs e)
+        {
+            AppConfig["location"] = $"{Location.X},{Location.Y}";
+            PropertiesHelper.Save(Path, AppConfig);
+        }
+        private void LabelNo1_Click(object sender, EventArgs e)
+        {
+            AppConfig["location"] = $"{Location.X},{Location.Y}";
+            PropertiesHelper.Save(Path, AppConfig);
+        }
+
+        private void OnMouseUp(object sender, MouseEventArgs e)
+        {
+            AppConfig["location"] = $"{Location.X},{Location.Y}";
+            PropertiesHelper.Save(Path, AppConfig);
+        }
+        private void OnMouseDown(object sender, MouseEventArgs e)
+        {
+            base.OnMouseDown(e);
+            FrmMain_MouseDown(this, e);
+        }
+    }
+}
