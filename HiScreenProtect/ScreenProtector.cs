22a4b51a76c4d439214b95949d2d5eb1e1e47643
1003 fengye <mcfengye1003@outlook.com>
2022-05-06 22:07:31 +0800

    -Now admin permission is required in order to install fonts and add start-up scripts. -Screen Protector added (half finished)

---
+++ HiScreenProtect/ScreenProtector.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Printing;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HiScreenProtect
+{
+    public partial class ScreenProtector : Form
+    {
+        public ScreenProtector()
+        {
+            InitializeComponent();
+            CheckForIllegalCrossThreadCalls = false;
+        }
+
+        private void CloseSlowly(object sender, FormClosingEventArgs e)
+        {
+            //this.Opacity = 1;
+            //for (int i = 0; i < 100; i++)
+            //{
+            //    Thread.Sleep(1);
+            //    Opacity -= 0.01;
+            //}
+            AnimateWindow(this.Handle, 500, AW_BLEND | AW_HIDE);
+        }
+        void CloseWindow()
+        {
+
+        }
+
+        private void ScreenProtector_Load(object sender, EventArgs e)
+        {
+            this.Opacity = 0;
+            for (int i = 0; i < 100; i++)
+            {
+                Thread.Sleep(1);
+                Opacity += 0.01;
+            }
+        }
+
+        private void CloseWindow(object sender, EventArgs e)
+        {
+            Close();
+        }
+        [DllImport("user32.dll")]//导入dll
+        private static extern bool AnimateWindow(IntPtr whnd, int dwtime, int dwflag);
+        //dwflag的取值如下
+        public const Int32 AW_HOR_POSITIVE = 0x00000001;
+        //从左到右显示
+        public const Int32 AW_HOR_NEGATIVE = 0x00000002;
+        //从右到左显示
+        public const Int32 AW_VER_POSITIVE = 0x00000004;
+        //从上到下显示
+        public const Int32 AW_VER_NEGATIVE = 0x00000008;
+        //从下到上显示
+        public const Int32 AW_CENTER = 0x00000010;
+        //若使用了AW_HIDE标志，则使窗口向内重叠，即收缩窗口；否则使窗口向外扩展，即展开窗口
+        public const Int32 AW_HIDE = 0x00010000;
+        //隐藏窗口，缺省则显示窗口
+        public const Int32 AW_ACTIVATE = 0x00020000;
+        //激活窗口。在使用了AW_HIDE标志后不能使用这个标志
+        public const Int32 AW_SLIDE = 0x00040000;
+        //使用滑动类型。缺省则为滚动动画类型。当使用AW_CENTER标志时，这个标志就被忽略
+        public const Int32 AW_BLEND = 0x00080000;
+        //透明度从高到低
+    }
+}
